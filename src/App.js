54ecab51084228a15b960c51acfb36853f4b992c
Williamrobert Green <wkgreen13@gmail.com>
2019-07-01 11:32:21 -0400

    adds login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import auth from 'solid-auth-client';
+
+import './App.css';
 
 function App() {
+
+  let [user, setUser] = useState();
+
+  async function login() {
+    let session = await auth.currentSession();
+    let popupUri = 'https://solid.community/common/popup.html';
+    if (!session)
+      session = await auth.popupLogin({ popupUri });
+    console.info("Welcome,", session.webId);
+    setUser(session.webId)
+  }
+
+  useEffect(() => {
+    if (!user) login()
+  }, [user])
+
   return (
     <div className="App">
       <Header>
-        <h3>Munny</h3>
+        <h3>Munny Pouch</h3>
+        <span className="spacer" />
+        <i className="material-icons click">person</i>
       </Header>
+      { user && <p>Welcome { user }</p> }
     </div>
   );
 }
