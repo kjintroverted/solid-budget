c4746d99802faa8f42073e55c5f77219c457f79b
Williamrobert Green <wkgreen13@gmail.com>
2019-07-01 15:35:52 -0400

    adds set ability for name on profile

--- src/comp/Profile.js
+++ src/comp/Profile.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import { getField } from '../util/pods'
+import { getField, setField } from '../util/pods'
 
 const Profile = ({ userID }) => {
 
   let [user, setUser] = useState();
+
+  let updateName = async (event, value) => {
+    event.preventDefault();
+    console.log("setting name", value);
+    await setField(userID, "fn", value);
+    setUser(value);
+  }
 
   // GET USER STORE ON LOAD
   useEffect(() => {
@@ -15,10 +22,12 @@ const Profile = ({ userID }) => {
     }())
   }, []);
 
+  let input;
   return (
     <ProfileView>
-      <h1>{ user }</h1>
-      <p>{ userID }</p>
+      <form onSubmit={ e => updateName(e, input.value) }>
+        <input ref={ ref => input = ref } defaultValue={ user } />
+      </form>
     </ProfileView>
   )
 }
@@ -26,6 +35,6 @@ const Profile = ({ userID }) => {
 export default Profile;
 
 const ProfileView = styled.div`
-      display: flex;
-      flex-direction: column;
+          display: flex;
+          flex-direction: column;
 `
--- src/util/pods.js
+++ src/util/pods.js
@@ -1,12 +1,25 @@
 import rdf from 'rdflib';
 
 const VCARD = rdf.Namespace("http://www.w3.org/2006/vcard/ns#")
+const LDP = rdf.Namespace("http://www.w3.org/ns/ldp#")
 const store = new rdf.IndexedFormula;
+const fetcher = new rdf.Fetcher(store);
+const updater = new rdf.UpdateManager(store);
 
 export const getField = async (userID, field) => {
   const userStore = store.sym(userID);
-  const doc = userStore.doc();
-  const fetcher = new rdf.Fetcher(store);
-  await fetcher.load(doc);
+  const profile = userStore.doc();
+  await fetcher.load(profile);
   return store.any(userStore, VCARD(field));
+}
+
+export const setField = async (userID, field, value) => {
+  const userStore = store.sym(userID);
+  const profile = userStore.doc();
+  const ins = rdf.st(userStore, VCARD(field), value, profile);
+  const del = store.statementsMatching(userStore, VCARD(field), null, profile);
+
+  updater.update(del, ins, (uri, ok) => {
+    if (!ok) console.error("Failed update.", uri);
+  })
 }
