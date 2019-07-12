895d97fcd74164c929802840e66f0eb03cef84fb
Williamrobert Green <wkgreen13@gmail.com>
2019-07-12 10:08:04 -0400

    adds ability to delete accounts

--- src/comp/AccountBreakdown.js
+++ src/comp/AccountBreakdown.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { IconButton, Input } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import styled from 'styled-components';
 
 import { ActionBar, WidgetContainer, HeaderBar, Spacer } from './ThemeComp';
@@ -9,11 +9,16 @@ import { saveFile, loadFile } from '../util/pods';
 export default ({ file }) => {
   let [accounts, updateAccounts] = useState([]);
   let [isAdding, setAdding] = useState(false);
+  let [isEditing, setEditing] = useState(false);
 
   async function loadAccounts() {
     let loadedAccounts = await loadFile(file, []);
     updateAccounts(loadedAccounts);
     console.log('accounts', loadedAccounts);
+  }
+
+  async function deleteAccount(i) {
+    updateAccounts([...accounts.slice(0, i), ...accounts.slice(i+1)]);
   }
 
   useEffect(() => {
@@ -34,18 +39,32 @@ export default ({ file }) => {
           <IconButton onClick={ () => setAdding(!isAdding) }>
             <i className="material-icons">{ isAdding ? 'close' : 'add' }</i>
           </IconButton>
+          <IconButton onClick={ () => setEditing(!isEditing) }>
+            <i className="material-icons">{ isEditing ? 'close' : 'edit' }</i>
+          </IconButton>
         </ActionBar>
       </HeaderBar>
+
+      {/* NO ACCOUNTS TO DISPLAY */ }
       { !accounts.length && "No Accounts to display." }
+
+      {/* MAIN ACCOUNT DISPLAY */ }
       {
         accounts.map((acc, i) => (
           <AccountView key={ `account-${ i }` }>
             <p>{ acc.name } ({ acc.label })</p>
             <Spacer />
             <p>{ acc.balance }</p>
+            { isEditing && // DELETE BUTTON
+              <IconButton color='secondary' onClick={ () => deleteAccount(i) }>
+                <i className="material-icons">delete</i>
+              </IconButton>
+            }
           </AccountView>
         ))
       }
+
+      {/* DISPLAY FORM FOR NEW ACCOUNT */ }
       { isAdding &&
         <AccountForm
           onSubmit={ account => {
