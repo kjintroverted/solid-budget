aa1d02213991b97754e7878fa55db8c8281d8c5a
Williamrobert Green <wkgreen13@gmail.com>
2020-03-04 10:20:40 -0500

    adds bar with last updated info fixes #34

---
+++ src/components/Warning.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { day } from '../util/helper';
+import { RowCenter } from './theme/ThemeComp';
+
+const Warning = () => {
+  let timestamp = localStorage.getItem("lastUpdated")
+  let lastUpdated = timestamp ? new Date(timestamp) : new Date(0)
+  let now = new Date()
+
+  if (now - lastUpdated < day * 2) {
+    let Bar = appBar("green");
+    return (
+      <Bar>
+        <RowCenter>
+          <i style={ { margin: "0px 5px" } } class="material-icons">check</i>
+          <p>
+            Budget is up to date.
+          </p>
+        </RowCenter>
+      </Bar>
+    )
+  }
+
+  let Bar = appBar(now - lastUpdated > day * 5 ? 'red' : 'orange')
+
+  return (
+    <Bar>
+      <RowCenter>
+        <i style={ { margin: "0px 5px" } } class="material-icons">warning</i>
+        <p>
+          Info displayed may be old. Last updated { Math.floor((now - lastUpdated) / day) } days ago.
+        </p>
+      </RowCenter>
+    </Bar>
+  )
+}
+
+export default Warning;
+
+function appBar(color) {
+  return styled.div`
+  width: 100vw;
+  padding: 0px 5px;
+  background: ${color };
+  color: white;
+`
+}
