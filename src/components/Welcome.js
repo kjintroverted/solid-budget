1556a58a466a6172f8b891d6e11b7b1ba0500808
Williamrobert Green <wkgreen13@gmail.com>
2020-04-10 15:35:26 -0400

    allows login and register from home page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Row } from './theme/ThemeComp'
 import { Button } from '@material-ui/core'
+import { popupLogin } from '../util/pods';
 
 const Welcome = () => (
   <Container>
@@ -10,8 +11,8 @@ const Welcome = () => (
     <p>This is a budgeting tool designed to help you understand where your money is allocated and going on a monthly or yearly basis. The idea is to make sure every dollar has a job and that you know what those jobs are to better plan your finances.</p>
     <p><i>We don't use your data!</i> Your personal data is not kept by this application. Instead you own and manage it all yourself using a user "Pod" which you can read more about <a href="https://solid.inrupt.com/how-it-works">here</a>.</p>
     <Row just="flex-end" align="center">
-      <Button variant="contained" color="primary">Login</Button>
-      <Button color="secondary">Register</Button>
+      <Button variant="contained" color="primary" onClick={ popupLogin }>Login</Button>
+      <Button color="secondary" href="https://inrupt.net/register">Register</Button>
     </Row>
   </Container>
 )
