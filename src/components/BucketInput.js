c24d867a9c05374d9c6b0839d8ecf5bf21f81926
Williamrobert Green <wkgreen13@gmail.com>
2020-06-11 15:58:50 -0400

    adds toggle for quick add/subtract

--- src/components/BucketInput.js
+++ src/components/BucketInput.js
@@ -4,31 +4,49 @@ import { Column, Row } from './theme/ThemeComp';
 
 const BucketInput = ({ value, update }) => {
   const [number, setNumber] = useState()
+  const [expand, setExpand] = useState(false)
 
   return (
     <Column align="flex-end" style={ { marginBottom: 15, marginTop: 5 } }>
-      <Input
-        type='number'
-        placeholder="0"
-        value={ value }
-        onChange={ e => update(+e.target.value) }
-      />
       <Row>
-        <IconButton size="small" color="primary"
-          onClick={ () => {
-            update(value + number)
-            setNumber(0)
-          } }
-        >
-          <i className="material-icons">add</i>
-        </IconButton>
-        <input
-          minor="true"
+        <Input
           type='number'
-          value={ number || '' }
-          onChange={ e => setNumber(+e.target.value) }
+          placeholder="0"
+          value={ value }
+          onChange={ e => update(+e.target.value) }
         />
+        <IconButton size="small" color="default"
+          onClick={ () => setExpand(!expand) }
+        >
+          <i className="material-icons">{ expand ? "cancel_presentation" : "calculate" }</i>
+        </IconButton>
       </Row>
+      { expand &&
+        <Row>
+          <IconButton size="small" color="primary"
+            onClick={ () => {
+              update(value + number)
+              setNumber(0)
+            } }
+          >
+            <i className="material-icons">add</i>
+          </IconButton>
+          <input
+            minor="true"
+            type='number'
+            value={ number || '' }
+            onChange={ e => setNumber(+e.target.value) }
+          />
+          <IconButton size="small" color="primary"
+            onClick={ () => {
+              update(value - number)
+              setNumber(0)
+            } }
+          >
+            <i className="material-icons">remove</i>
+          </IconButton>
+        </Row>
+      }
     </Column>
   )
 }
