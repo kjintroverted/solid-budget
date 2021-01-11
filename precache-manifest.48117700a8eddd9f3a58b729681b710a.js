ce0b5c54b91a785a8532b09c5926e50fcf7f696c
CircleCI <wkgreen13@gmail.com>
2021-01-11 18:29:13 +0000

    CI Deploy

---
+++ precache-manifest.48117700a8eddd9f3a58b729681b710a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce168020eb59e8481d90551d8bd4478",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70c7b66aeac1c7a994ba",
+    "url": "/static/css/main.4f03dec5.chunk.css"
+  },
+  {
+    "revision": "079f44ea657854efb9f5",
+    "url": "/static/js/2.8e748075.chunk.js"
+  },
+  {
+    "revision": "21ba1ade3f0c71addfd3d1f330324c2c",
+    "url": "/static/js/2.8e748075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c7b66aeac1c7a994ba",
+    "url": "/static/js/main.627c8182.chunk.js"
+  },
+  {
+    "revision": "f8f1cdbdcf11f13e5908",
+    "url": "/static/js/runtime-main.858d657e.js"
+  },
+  {
+    "revision": "e0a514f3be2689695b0f2df73218a15d",
+    "url": "/static/media/munny_pouch.e0a514f3.png"
+  }
+]);
