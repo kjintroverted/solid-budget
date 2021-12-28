83f641427e55dafdcdd3539450aeae7eaf92247b
kjintroverted <wkgreen13@gmail.com>
2021-12-27 21:49:36 -0500

    allows update on accounts

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,34 @@
-import { IconButton } from "@material-ui/core";
+import { Fab, IconButton } from "@material-ui/core";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { HeaderBar, Spacer } from "solid-core/dist/components/styled";
+import { HeaderBar, SaveButton, Spacer } from "solid-core/dist/components/styled";
+import { loadThing, nameFilter, SaveState } from "solid-core/dist/pods";
 import styled from "styled-components";
 import { THEME } from "../util";
 import Accounts from "./accounts/Accounts";
+import { accountStruct } from "./accounts/accountStruct";
 
 const Dashboard = ({ user, data }) => {
+
+  const { queue, saveFromQ } = useContext(SaveState);
+
+  const [accounts, setAccounts] = useState([])
+
+  useEffect(() => {
+    if (!data) return;
+    loadAccounts(data)
+      .then(setAccounts)
+  }, [data])
+
+  async function loadAccounts(things) {
+    // GET ALL RECIPE DATA
+    return await Promise.all(
+      things
+        .filter(nameFilter('account'))
+        .map(t => loadThing(t.url, accountStruct))
+    );
+  }
+
   return (
     <Layout>
       <HeaderBar theme={ THEME }>
@@ -16,8 +39,18 @@ const Dashboard = ({ user, data }) => {
         </Link>
       </HeaderBar>
       <Content>
-        <Accounts />
+        <Accounts data={ accounts } />
       </Content>
+      {
+        !!queue.length &&
+        <SaveButton>
+          <Fab
+            color="secondary"
+            onClick={ saveFromQ }>
+            <span className="material-icons">save</span>
+          </Fab>
+        </SaveButton>
+      }
     </Layout>
   )
 }
--- src/components/accounts/Accounts.js
+++ src/components/accounts/Accounts.js
@@ -1,25 +1,37 @@
-import { IconButton } from "@material-ui/core";
-import { useEffect, useState } from "react";
+import { IconButton, Input } from "@material-ui/core";
+import { useContext, useEffect, useState } from "react";
 import { Card, Divider, Pane, Row, Spacer, Title } from "solid-core/dist/components/styled";
-import { initThing } from "solid-core/dist/pods";
+import { initThing, setAttr, addToUpdateQueue, SaveState } from "solid-core/dist/pods";
 import { THEME } from "../../util";
 import AccountForm from "./AccountForm";
 import { accountStruct } from "./accountStruct";
 
 const Accounts = ({ data }) => {
 
+  const { queue, updateQueue } = useContext(SaveState);
+
   const [isAdding, setIsAdding] = useState(false)
   const [accounts, updateAccounts] = useState([])
 
   useEffect(() => {
-    if (data) updateAccounts(data)
+    if (data) {
+      updateAccounts(data.sort((a) => a.primary ? -1 : 0))
+    }
   }, [data])
 
   async function addAccount(acc) {
-    debugger
     setIsAdding(false)
     await initThing('account', acc, accountStruct)
     updateAccounts([...accounts, acc])
+  }
+
+  function updateAccount(acc, field) {
+    let i = accounts.findIndex(a => a.thing.url === acc.thing.url)
+    return e => {
+      let t = setAttr(acc.thing, accountStruct[field], e.target.value)
+      updateQueue(addToUpdateQueue(queue, t))
+      updateAccounts([...accounts.slice(0, i), { ...acc, [field]: e.target.value, thing: t }, ...accounts.slice(i + 1)])
+    }
   }
 
   return (
@@ -38,7 +50,18 @@ const Accounts = ({ data }) => {
         }
         {
           accounts.map(a => (
-            <Title>{ a.title }</Title>
+            <>
+              <Row key={ a.thing.url ? a.thing.url : a.title }>
+                <Title style={ { margin: 0 } }>{ a.title }</Title>
+                <Spacer />
+                <Input
+                  onChange={ updateAccount(a, 'balance') }
+                  style={ { width: '6em' } }
+                  type="number"
+                  value={ a.balance } />
+              </Row>
+              <Divider style={ { borderBottomWidth: '0px' } } theme={ THEME } />
+            </>
           ))
         }
       </Card>
