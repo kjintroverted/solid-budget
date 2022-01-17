fc4473e1c5ecdbf2b8d9b669cffaa9df7807d5db
kjintroverted <wkgreen13@gmail.com>
2022-01-17 15:26:22 -0500

    adds init note form;

---
+++ src/components/notes/noteStruct.js
@@ -0,0 +1,18 @@
+import { FOAF } from "@inrupt/vocab-common-rdf";
+import { getAndParse, stringifyAndSet } from "solid-core/dist/pods";
+
+export const notebookStruct = {
+  notes: {
+    predicate: FOAF.Document,
+    parse: getAndParse,
+    set: stringifyAndSet
+  }
+}
+
+const note = {
+  text: "Message text.",
+  actionType: "TRAN/UP",
+  account: "url",
+  target: "url",
+  recurring: false
+}
