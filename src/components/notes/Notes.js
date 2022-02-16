911169806996ba7e7d4cfa1bb8a62e6f8015dbe3
kjintroverted <wkgreen13@gmail.com>
2022-02-16 16:09:18 -0500

    adds default action button

--- src/components/notes/Notes.js
+++ src/components/notes/Notes.js
@@ -1,4 +1,4 @@
-import { IconButton } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import { useContext, useEffect, useState } from "react";
 import { CardHeader, Pane, Row, Spacer } from "solid-core/dist/components/styled";
 import { addToUpdateQueue, initThing, loadByName, SaveState, setAllAttr } from "solid-core/dist/pods";
@@ -36,6 +36,19 @@ const Notes = () => {
     updateNotes({ ...updatedBook, thing })
   }
 
+  function removeNote(i) {
+    let updatedBook = { ...notebook, notes: [...notebook.notes.slice(0, i), ...notebook.notes.slice(i + 1)] }
+    let thing = setAllAttr(updatedBook.thing, updatedBook);
+    updateQueue(addToUpdateQueue(queue, thing))
+    updateNotes({ ...updatedBook, thing })
+  }
+
+  function complete(i) {
+    return () => {
+      removeNote(i)
+    }
+  }
+
   return (
     <Pane width="100%">
       <Row align="center">
@@ -49,9 +62,13 @@ const Notes = () => {
         isAdding && <NoteForm onSubmit={ addNote } />
       }
       {
-        notebook.notes.map(note => (
+        notebook.notes.map((note, i) => (
           <NoteCard key={ note.text }>
-            { note.text }
+            <Row align='center'>
+              { note.text }
+              <Spacer />
+              <Button color='secondary' onClick={ complete(i) }>Done</Button>
+            </Row>
           </NoteCard>
         ))
       }
